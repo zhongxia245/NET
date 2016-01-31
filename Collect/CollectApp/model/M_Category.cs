38d7c3b2b7cb336955bc7b2f1545362fd206e2ab
zhongxia245 <zhongxia245@sina.com>
2016-01-31 13:25:06 +0800

    add lib

---
+++ Collect/CollectApp/model/M_Category.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectApp.model
+{
+    /// <summary>
+    /// 类别实体
+    /// </summary>
+    public class M_Category
+    {
+        /// <summary>
+        /// 类别名称
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 请求参数
+        /// </summary>
+        public string Param { get; set; }
+        /// <summary>
+        /// 属于第几期
+        /// </summary>
+        public int TimePhase { get; set; }
+
+        /// <summary>
+        /// 父类别名称
+        /// </summary>
+        public string ParentTitle { get; set; }
+    }
+}
---
+++ Collect/CollectApp/services/S_Core.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace CollectApp.services
+{
+    /// <summary>
+    /// 业务逻辑的核心代码
+    /// </summary>
+    public class S_Core
+    {
+        /// <summary>
+        /// 合并表格，把英文名和中文名合并在一个表格里面
+        /// </summary>
+        /// <param name="dt1"></param>
+        /// <param name="dt2"></param>
+        /// <returns></returns>
+        public static DataTable MergeTableColumn(DataTable dt1,DataTable dt2) {
+
+            for (int i = 0; i < dt1.Rows.Count; i++)
+            {
+                //for (int j = 0; j < dt2.Rows.Count; j++)
+                //{
+                //    if (dt1.Rows[i]["id"].Equals(dt2.Rows[j]["id"]))
+                //    {
+                //        dt1.Rows[i]["en"] = dt2.Rows[j]["en"];
+                //        break;
+                //    }
+                //    //if (i < dt2.Rows.Count)
+                //    //{
+                //    //    dt1.Rows[i]["en"] = dt2.Rows[i]["en"];
+                //    //}
+                //    //else
+                //    //{
+                //    //    dt1.Rows[i]["en"] = "No English Name";
+                //    //}
+                //}
+                //dt1.Rows[i]["en"] = "No English Name";
+
+                if (i < dt2.Rows.Count)
+                {
+                    dt1.Rows[i]["en"] = dt2.Rows[i]["en"];
+                }
+                else
+                {
+                    dt1.Rows[i]["en"] = "No English Name";
+                }
+            }
+
+            return dt1;
+        }
+    }
+}
